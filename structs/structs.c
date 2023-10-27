525f459ec179ee3d0972963fb33984ee11e03b7f
Gonzalo Campos <gonzalocampose1@gmail.com>
2023-10-26 20:58:24 -0600

    Use malloc to allocate memory.

--- structs/structs.c
+++ structs/structs.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     double x;
     double y;
 } point;
+
+point *makepoint(double x, double y) {
+    point *p = (point*)malloc(sizeof(point));
+    p->x = x;
+    p->y = y;
+    return p;
+}
 
 int main() {
 
@@ -13,8 +21,11 @@ int main() {
     center.x = 10;
     center.y = 10;
 
+    point *top_left = makepoint(33.0, -10.0);
+
     printf("(x, y) = (%.1f, %.1f)\n", zero.x, zero.y);
     printf("(x, y) = (%.1f, %.1f)\n", center.x, center.y);
+    printf("(x, y) = (%.1f, %.1f)\n", top_left->x, top_left->y);
     return 0;
 }
 
